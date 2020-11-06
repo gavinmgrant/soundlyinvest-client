6de9b3076bdbca210b6531a47a530c359623644b
Gavin Grant <gavingrant@gmail.com>
2020-11-06 15:21:10 -0800

    refactor(units): add to labels in forms

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,45 +15,45 @@ class Expenses extends Component {
                 <section>
                     <section>
                         <label for="property-tax-rate">Property Tax Rate (%): </label>
-                        <span data-tip="Enter the whole number. If 1%, enter 1, not 0.01.">{InfoCircle}</span>
+                        <span data-tip="Enter a whole number. If 1%, enter 1, not 0.01.">{InfoCircle}</span>
                         <ReactTooltip />
                         <input type="number" name="property-tax-rate" min="0"/>
                     </section>
                     <section>
-                        <label for="property-manager">Property Manager: </label>
+                        <label for="property-manager">Property Manager ($): </label>
                         <span data-tip="If you have a property manager, enter monthly fee here.">{InfoCircle}</span>
                         <ReactTooltip />
                         <input type="number" name="unit-2" min="0"/>
                     </section>
                     <section>
-                        <label for="insurance">Insurance: </label>
+                        <label for="insurance">Insurance ($): </label>
                         <span data-tip="Enter total cost for insurance such as hazard and umbrella.">{InfoCircle}</span>
                         <ReactTooltip />
                         <input type="number" name="insurance" min="0"/>
                     </section>
                     <section>
-                        <label for="utilities">Utilities: </label>
+                        <label for="utilities">Utilities ($): </label>
                         <span data-tip="Enter all utility expenses paid by the landlord.">{InfoCircle}</span>
                         <ReactTooltip />
                         <input type="number" name="utilities" min="0"/>
                     </section>
                     <section>
-                        <label for="gardener">Gardener: </label>
+                        <label for="gardener">Gardener ($): </label>
                         <span data-tip="Enter amount reserved for managing the landscape.">{InfoCircle}</span>
                         <ReactTooltip />
                         <input type="number" name="gardener" min="0"/>
                     </section>
                     <section>
-                        <label for="maintenance">Maintenance: </label>
+                        <label for="maintenance">Maintenance ($): </label>
                         <span data-tip="Enter miscellaneous maintenance expenses here.">{InfoCircle}</span>
                         <ReactTooltip />
                         <input type="number" name="maintenance" min="0"/>
                     </section>
                     <section>
-                        <label for="vacancy">Vacancy: </label>
-                        <span data-tip="Account for lost income as tenants turn over.">{InfoCircle}</span>
+                        <label for="vacancy-rate">Vacancy Rate (%): </label>
+                        <span data-tip="Enter a whole number. This is a percentage of monthly income.">{InfoCircle}</span>
                         <ReactTooltip />
-                        <input type="number" name="vacancy" min="0"/>
+                        <input type="number" name="vacancy-rate" min="0" placeholder="2% is a good starting point."/>
                     </section>
                 </section>
                 <Link to="/loan">
--- src/components/Loan/Loan.js
+++ src/components/Loan/Loan.js
@@ -14,18 +14,22 @@ class Loan extends Component {
             <form>
                 <section>
                     <section>
-                        <label for="down-payment">Down Payment Percentage: </label>
+                        <label for="down-payment">Down Payment (%): </label>
+                        <span data-tip="Enter a whole number. If 20%, enter 20, not 0.20.">{InfoCircle}</span>
+                        <ReactTooltip />
                         <input type="number" name="down-payment" min="0"/>
                     </section>
                     <section>
-                        <label for="interest-rate">Interest Rate Percentage: </label>
+                        <label for="interest-rate">Interest Rate (%): </label>
+                        <span data-tip="Enter a whole number. If 4%, enter 4, not 0.04.">{InfoCircle}</span>
+                        <ReactTooltip />
                         <input type="number" name="interest-rate" min="0"/>
                     </section>
                     <section>
                         <label for="loan-period">Loan Period (months): </label>
-                        <span data-tip="Typically 360 months (30 years).">{InfoCircle}</span>
+                        <span data-tip="Enter the number of months until loan is paid off.">{InfoCircle}</span>
                         <ReactTooltip />
-                        <input type="number" name="loan-period" min="0"/>
+                        <input type="number" name="loan-period" min="0" placeholder="360 months is a good starting point."/>
                     </section>
                 </section>
                 <Link to="/goals">
